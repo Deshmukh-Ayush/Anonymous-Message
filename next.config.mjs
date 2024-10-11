98f17e56f6939bd271152141de4d7db750541bb3
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-10-11 17:00:17 +0530

    signUp and otp done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    webpack: (config) => {
+      config.resolve.modules.push(path.join(__dirname, 'emails'));
+      return config;
+    },
+  };
+
 export default nextConfig;
