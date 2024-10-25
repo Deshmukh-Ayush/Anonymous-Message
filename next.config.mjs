01b8c68eb2878644a40ce81e56be58393717b2d6
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-10-25 23:35:52 +0530

    home page error fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    webpack: (config) => {
-      config.resolve.modules.push(path.join(__dirname, 'emails'));
-      return config;
-    },
-  };
+// module.exports = {
+//     webpack: (config) => {
+//       config.resolve.modules.push(path.join(__dirname, 'emails'));
+//       return config;
+//     },
+//   };
 
 export default nextConfig;
