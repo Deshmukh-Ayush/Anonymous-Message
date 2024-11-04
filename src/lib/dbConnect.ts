2b405ee1b1b1e49130a67d20b67ed407c684eb36
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-11-04 18:18:04 +0530

    database connection success and email verify code success

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,30 +1,28 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 
 type ConnectionObject = {
-    isConnected?: number
-}
+  isConnected?: number;
+};
 
-const connection: ConnectionObject = {}
+const connection: ConnectionObject = {};
 
 async function dbConnect(): Promise<void> {
-    if (connection.isConnected) {
-        console.log("Already Connected to Database");
-        return
-    }
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
+  }
 
-    try {
-        const db = await mongoose.connect(process.env.MONGODV_URI || '', {})
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {})
 
-        connection.isConnected = db.connections[0].readyState
+    connection.isConnected = db.connections[0].readyState;
 
-        console.log("DB connected successfully");
-        
-    } catch (error) {
-        console.log("Database connection failed", error)
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
 
-        process.exit(1)
-    }
+    process.exit(1);
+  }
 }
 
 export default dbConnect;
