fb80bde35f089b81e9e2f9bc3a3b8e8b708ce6a4
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-11-07 23:22:58 +0530

    verify form layout

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+import { Button } from '@/components/ui/button';
+import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
 import { verifySchema } from '@/schemas/verifySchema';
 import { ApiResponse } from '@/types/ApiResponse';
@@ -42,7 +47,30 @@ const VerifyAccount = () => {
         }
     }
   return (
-    <div>VerifyAccount</div>
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+        <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md'>
+            <div className='text-center'>
+                <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl mb-6'>Verify your account</h1>
+                <p className='mb-4'>Enter the verfication code sent to your Email</p>
+            </div>
+            <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Verify</Button>
+          </form>
+        </Form>
+        </div>
+    </div>
   )
 }
 
