7d91e1effd6dc83bd4d9f2de5dd8f33076c6a0e0
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-11-08 23:03:48 +0530

    minor bug fixes in sign-in and document formatting

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,59 +1,66 @@
-"use client"
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { Form, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { useToast } from '@/hooks/use-toast';
-import { verifySchema } from '@/schemas/verifySchema';
-import { ApiResponse } from '@/types/ApiResponse';
-import { zodResolver } from '@hookform/resolvers/zod';
-import axios, { AxiosError } from 'axios';
-import { useParams, useRouter } from 'next/navigation'
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import * as z from 'zod';
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
+import { verifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 const VerifyAccount = () => {
-    const router = useRouter();
-    const params = useParams<{username: string}>()
-    const { toast } = useToast()
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
 
-    const form = useForm<z.infer<typeof verifySchema>>({
-        resolver: zodResolver(verifySchema),
-        
-      })
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
 
-    const onSubmit = async (data: z.infer<typeof verifySchema>) => {
-        try {
-            const response = await axios.post('/api/verify-code', {
-                usrename: params.username,
-                code: data.code
-            })
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+      const response = await axios.post("/api/verify-code", {
+        usrename: params.username,
+        code: data.code,
+      });
 
-            toast({
-                title: "Success",
-                description: response.data.message
-            })
+      toast({
+        title: "Success",
+        description: response.data.message,
+      });
 
-            router.replace('sign-in')
-        } catch (error) {
-            console.error("Error in signup of user", error)
-            const axiosError = error as AxiosError<ApiResponse>
-                toast({
-                    title: "Signup failed",
-                    description: axiosError.response?.data.message,
-                    variant: "destructive"
-                })
-        }
+      router.replace("sign-in");
+    } catch (error) {
+      console.error("Error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Signup failed",
+        description: axiosError.response?.data.message,
+        variant: "destructive",
+      });
     }
+  };
   return (
-    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
-        <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md'>
-            <div className='text-center'>
-                <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl mb-6'>Verify your account</h1>
-                <p className='mb-4'>Enter the verfication code sent to your Email</p>
-            </div>
-            <Form {...form}>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify your account
+          </h1>
+          <p className="mb-4">Enter the verfication code sent to your Email</p>
+        </div>
+        <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
             <FormField
               name="code"
@@ -69,9 +76,9 @@ const VerifyAccount = () => {
             <Button type="submit">Verify</Button>
           </form>
         </Form>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default VerifyAccount
+export default VerifyAccount;
