d1bca440ca3bb470758528f8f0ae76b79f9eee6a
Ayush Deshmukh <ayushdeshmukh301@gmail.com>
2024-11-11 23:10:51 +0530

    dummy carausel set

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import messages from "@/messages.json";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+      <section className="text-center mb-8 md:mb-12">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Dive into the World of Anonymous Conversations
+        </h1>
+        <p className="mt-3 md:mt-4 text-base md:text-lg">
+          Explore Mystery Message - Where your identity remains a secret.
+        </p>
+      </section>
 
-export default Home
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </main>
+  );
+};
+
+export default Home;
